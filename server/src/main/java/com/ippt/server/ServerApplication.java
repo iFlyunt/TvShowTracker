6c46b8120903a0acd11c91e3aefedf127c925b75
Igor Flyunt <igflyunt@gmail.com>
2018-05-01 17:21:19 +0300

    change poms files and project structure

--- server/src/main/java/com/ippt/server/ServerApplication.java
+++ server/src/main/java/com/ippt/server/ServerApplication.java
@@ -1,10 +1,7 @@
 package com.ippt.server;
 
-import com.ippt.server.repository.WatchedEpisodeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class ServerApplication {
